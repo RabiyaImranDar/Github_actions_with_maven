7ab86e3cedc7cb7fb00be26faf680f8d847ba18c
Rabiya Dar <rabiya_khalid@hotmail.com>
2025-11-19 01:35:00 +0100

    uploading repository with commit

---
+++ src/test/java/practice_demo/AppTest.java
@@ -0,0 +1,13 @@
+package practice_demo;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+
+    @Test
+    void demoTest() {
+        assertTrue(true);
+    }
+}
